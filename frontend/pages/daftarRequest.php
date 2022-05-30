068e1fd5a9db949d38ceffbe93c5b592d327e000
Muhammad Riandi <62460140+riandiuniska@users.noreply.github.com>
2022-05-31 01:20:05 +0700

    update button konsultasi
    update button konsultasi (menuju chat group)

--- frontend/pages/daftarRequest.php
+++ frontend/pages/daftarRequest.php
@@ -394,7 +394,8 @@ $acception = $objAccept->getDataByIdStudent($_SESSION['id']);
                                 </td>
                                 <td class="border-b px-4 py-2 text-center">
                                     <?php if($key['status'] == 'active'){ ?>
-                                        <button type="button" class="px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Konsultasi</button>
+                                        <a href="http://localhost/websocket/group_chat.php">
+                                        <button type="button" class="px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Konsultasi</button></a>
                                     <?php } else {?>
                                         <p>-</p>
                                     <?php } ?>
